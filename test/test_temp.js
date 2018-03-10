896dff2976ca972711994d095b01bc525041c5a3
ando <ando9527@gmail.com>
2018-03-10 11:54:44 +0800

    fixed babelrc

--- test/test_temp.js
+++ test/test_temp.js
@@ -9,5 +9,16 @@ import { checkFields } from './utils'
 
 dotenv.load()
 
-const client = Cobinhood()
+const client = Cobinhood({apiSecret:process.env.BOT_API_SECRET})
 
+
+
+test.serial('[REST][AUTH] All of My Orders', async t => {
+    const param= {limit: '50'}
+    const param2= {limit: '50', page:3}
+    const mo = await client.myOrderSymbol(param2)
+    console.log(mo)
+    t.is(mo.success, true)
+
+  
+  })
